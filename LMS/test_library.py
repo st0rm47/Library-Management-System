29a14dd3e5d73fca225119caf35d442a4ef43978
Subodh Ghimire <93703891+st0rm47@users.noreply.github.com>
2024-02-05 21:25:28 +0545

    Test File

--- LMS/test_library.py
+++ LMS/test_library.py
@@ -15,12 +15,14 @@ def test_add_book2():
     book2 = Book("CA", "Kiran Bagale", "987654321" , True)
     lib.add_book(book2)
     assert lib.books == [book2]
-    
+
+
 #Testing to remove a book
 def test_remove_book():
     lib = Library()
     lib.remove_book("123456789")
     assert lib.books == []
+
 
 #Testing to add a member
 def test_add_member1():
@@ -29,19 +31,22 @@ def test_add_member1():
     lib.add_member(member1)
     assert lib.members == [member1]
 
+
 #Testing to add a member
 def test_add_member2():
     lib = Library()
     member2 = Member("Firoj", "2", "9860443448")
     lib.add_member(member2)
     assert lib.members == [member2]
-    
+
+
 #Testing to remove a member
 def test_remove_member():
     lib = Library()
     lib.remove_member("2")
     assert lib.members == []
-    
+
+
 #Testing to borrow a book
 def test_borrow_book():
     lib = Library()
@@ -51,6 +56,7 @@ def test_borrow_book():
     lib.add_member(member)
     lib.borrow_book("3", "789456123")
     assert book.availability == False
+
 
 #Testing to return a book   
 def test_return_book():
@@ -63,6 +69,7 @@ def test_return_book():
     lib.return_book("3", "789456123")
     assert book.availability == True
 
+
 #Testing to display a book   
 def test_display_book():
     lib = Library()
@@ -70,6 +77,7 @@ def test_display_book():
     lib.add_book(book)
     result = lib.display_book()
     assert result == "Title: NM, Author: Mohit Guragain, ISBN: 789456123, Availability: Available\n"
+
 
 #Testing to display a member
 def test_display_member():
